199e4ef85d51f5822598006b8ea3b104e95b5be5
samPuriwat <sam.puriwat@gmail.com>
2018-05-08 16:47:48 +0700

    Array Operator

---
+++ src/lab5/arrayOperator.java
@@ -0,0 +1,9 @@
+package lab5;
+//1. method for input data  (10)
+//2. method for show data in array
+//3. method to find average value in array
+//4. method to find maximum value in array
+//5. method to find minimum value in array
+public class arrayOperator {
+
+}
