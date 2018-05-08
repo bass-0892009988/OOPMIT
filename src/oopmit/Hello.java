085eff9b099bb2296300e7acd03881c7533704ea
samPuriwat <sam.puriwat@gmail.com>
2018-05-08 15:13:50 +0700

    update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello, My name is Puriwat");
+        System.out.println("Major: Information System");
+        System.out.println("Faculty: Management Technology");
+        System.out.println("University: RMUTSV");
     }//main
 
 }//class
