be55445112f05a1d455e86acc41370d0408c1a2a
samPuriwat <sam.puriwat@gmail.com>
2018-05-08 15:16:43 +0700

    delete data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
         System.out.println("Hello, My name is Puriwat");
         System.out.println("Major: Information System");
         System.out.println("Faculty: Management Technology");
-        System.out.println("University: RMUTSV");
+
     }//main
 
 }//class
