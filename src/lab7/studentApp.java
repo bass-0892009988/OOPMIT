52d56a84a3bdb8403b851dae406405e032f4ef5c
samPuriwat <sam.puriwat@gmail.com>
2018-05-15 14:17:03 +0700

    input data from user (Buffer)

--- src/lab7/studentApp.java
+++ src/lab7/studentApp.java
@@ -1,5 +1,8 @@
 package lab7;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class studentApp {
@@ -18,9 +21,26 @@ public class studentApp {
         //input data from user
         Student s3 = new Student();
         s3 = inputData(s3); //s
+        s3 = inputDataBuffer(s3);
         System.out.println(s3.toString());
 
     }//main
+
+    private static Student inputDataBuffer(Student s) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter  your information: ");
+        System.out.print("Name: ");
+        s.setName(reader.readLine());
+        System.out.print("Age: ");
+        s.setAge(Integer.parseInt(reader.readLine()));
+        System.out.print("Gender: ");
+        s.setGender(reader.readLine());
+        System.out.print("Height: ");
+        s.setHeight(Double.parseDouble(reader.readLine()));
+
+        return s;
+
+    }
 
     private static Student inputData(Student s) {
         Scanner sc = new Scanner(System.in);
